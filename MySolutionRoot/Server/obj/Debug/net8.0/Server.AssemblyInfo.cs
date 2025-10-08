39dd9e0c7463fdca268bf1957c25d7c50b4e7b8c
PumPururururum <obrigankenobi66@gmail.com>
2025-10-08 12:57:17 +0300

    cc

--- MySolutionRoot/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ MySolutionRoot/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a02ad28a9022bb719b268b6466e86d1502d8369d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e805e5e62e20a4ed1fd569cbbbb27f196811f477")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
